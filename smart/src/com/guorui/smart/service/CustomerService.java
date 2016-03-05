778e2092ea2170467e1bbfb1cb8442f6e8f15264
bonn19573 <bonn19573@hotmail.com>
2016-03-05 11:55:32 +0800

    db util version

--- smart/src/com/guorui/smart/service/CustomerService.java
+++ smart/src/com/guorui/smart/service/CustomerService.java
@@ -1,10 +1,5 @@
 package com.guorui.smart.service;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.guorui.smart.model.Customer;
@@ -17,33 +12,6 @@ public class CustomerService {
 		
 		List<Customer> customers = DatabaseUtil.queryEntityList(Customer.class, sql, new Object[]{});
 		
-//		List<Customer> customers = new ArrayList<Customer>();
-//		
-//		Connection connection = DatabaseUtil.getConnection();
-//		try {
-//			Statement statement = connection.createStatement();
-//			ResultSet rs = statement.executeQuery(sql);
-//			
-//			while(rs.next()){
-//				Customer customer = new Customer();
-//				customer.setId(rs.getInt("id"));
-//				customer.setName(rs.getString("name"));
-//				customer.setContact(rs.getString("contact"));
-//				customer.setTelephone(rs.getString("telephone"));
-//				customer.setEmail(rs.getString("email"));
-//				customer.setComment(rs.getString("remark"));
-//				customers.add(customer);
-//			}
-//			
-//			rs.close();
-//			statement.close();
-//			
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} finally{
-//			DatabaseUtil.closeConnection(connection);
-//		}
 		return customers;
 		
 	}
